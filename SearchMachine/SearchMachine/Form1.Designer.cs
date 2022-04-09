d66c4aca4c9100a8a5fccdd22ff39be2b3c83c43
kirdreamer <92848197+kirdreamer@users.noreply.github.com>
2022-04-09 18:35:42 +0300

    Add files via upload

---
+++ SearchMachine/SearchMachine/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace SearchMachine
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 
+
+        
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.AnswerLabel = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.pathOpen = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Search Word";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(498, 421);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(290, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Program was developed by Kirill Bulanov";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(190, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(497, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(693, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.AnswerLabel);
+            this.panel1.Location = new System.Drawing.Point(16, 155);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(771, 263);
+            this.panel1.TabIndex = 4;
+            // 
+            // AnswerLabel
+            // 
+            this.AnswerLabel.AutoSize = true;
+            this.AnswerLabel.Location = new System.Drawing.Point(4, 4);
+            this.AnswerLabel.Name = "AnswerLabel";
+            this.AnswerLabel.Size = new System.Drawing.Size(0, 13);
+            this.AnswerLabel.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1_FileOk);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Select a Dictionary";
+            // 
+            // pathBox
+            // 
+            this.pathBox.Enabled = false;
+            this.pathBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pathBox.Location = new System.Drawing.Point(190, 20);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(497, 26);
+            this.pathBox.TabIndex = 6;
+            // 
+            // pathOpen
+            // 
+            this.pathOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pathOpen.Location = new System.Drawing.Point(693, 18);
+            this.pathOpen.Name = "pathOpen";
+            this.pathOpen.Size = new System.Drawing.Size(94, 30);
+            this.pathOpen.TabIndex = 7;
+            this.pathOpen.Text = "Select";
+            this.pathOpen.UseVisualStyleBackColor = true;
+            this.pathOpen.Click += new System.EventHandler(this.PathOpen_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopButton.Location = new System.Drawing.Point(604, 114);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(184, 35);
+            this.stopButton.TabIndex = 8;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.pathOpen);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Search Machine";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label AnswerLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.Button pathOpen;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
+
---
+++ SearchMachine/SearchMachine/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SearchMachine
+{
+    public partial class Form1 : Form
+    {
+        bool stopFlag = false;
+        string searchingLine;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string path = pathBox.Text;
+            if (File.Exists(path))
+            {
+                using (FileStream fstream = File.OpenRead(path))
+                {
+                    stopFlag = false;
+                    long endPos = 0;
+                    long beginPos = 0;
+                    AnswerLabel.Text = "";
+                    searchingLine = textBox1.Text;
+                    button1.Enabled = false;
+                    pathOpen.Enabled = false;
+                    stopButton.Enabled = true;
+
+                    while (fstream.Position < fstream.Length)
+                    {
+                        if (stopFlag)
+                            break;
+                        bool flag = false;
+                        beginPos = endPos;
+                        int it = 0;
+
+                        byte[] ch = new byte[1];
+                        do
+                        {
+                            fstream.Read(ch, 0, 1);
+                            if ((!Encoding.Default.GetString(ch).Equals("\n") &&
+                                !Encoding.Default.GetString(ch).Equals("\r")) && !flag)
+                            {
+                                if (searchingLine.Length > 0)
+                                {
+                                    if (searchingLine[it].Equals(Encoding.Default.GetString(ch)[0]))
+                                        ++it;
+                                    if (it == searchingLine.Length)
+                                        flag = true;
+                                }
+                            }
+                        }
+                        while ((!Encoding.Default.GetString(ch).Equals("\n") &&
+                                !Encoding.Default.GetString(ch).Equals("\r")) && fstream.Position < fstream.Length);
+                        endPos = fstream.Position;
+                        if (flag)
+                        {
+                            fstream.Seek(beginPos - endPos, SeekOrigin.Current);
+                            byte[] output = new byte[endPos - beginPos - 1];
+                            fstream.Read(output, 0, output.Length);
+                            string textfile = Encoding.Default.GetString(output);
+                            AnswerLabel.Text += textfile + "\n";
+                            panel1.Refresh();
+                        }
+                        fstream.Seek(endPos, SeekOrigin.Begin);
+                        Application.DoEvents();
+
+                    }
+                    stopButton.Enabled = false;
+                    button1.Enabled = true;
+                    pathOpen.Enabled = true;
+                }
+            }
+        }
+
+        private void OpenFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void PathOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            pathBox.Text = "";
+            pathBox.Text += openFileDialog1.FileName;
+            MessageBox.Show("The following file was selected : " + openFileDialog1.FileName);
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            stopFlag = true;
+        }
+    }
+}
